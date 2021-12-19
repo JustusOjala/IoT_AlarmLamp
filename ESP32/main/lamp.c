f5a2ba4202d4fc94eeafea1242efa21d0c519eef
Justus Ojala <justus.ojala@aalto.fi>
2021-12-19 20:32:09 +0200

    Added button control to toggle on/off

--- ESP32/main/lamp.c
+++ ESP32/main/lamp.c
@@ -6,7 +6,7 @@
 #include "driver/ledc.h"
 #include "driver/adc.h"
 
-#define MOS_pin 25
+#define mos_pin 25
 
 void app_main(void)
 {
@@ -26,25 +26,33 @@ void app_main(void)
         .channel        = LEDC_CHANNEL_0,
         .timer_sel      = LEDC_TIMER_0,
         .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = MOS_pin,
+        .gpio_num       = mos_pin,
         .duty           = 0, // Set duty to 0%
         .hpoint         = 0
     };
     ESP_ERROR_CHECK(ledc_channel_config(&mos_channel));
 
     adc1_config_channel_atten(ADC1_CHANNEL_7, 3);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, 3);
     
     int pot = 0; int prev = 0;
     int brightness = 0; int on = 0;
     while(1) {
+        if(adc1_get_raw(ADC1_CHANNEL_6) > 2000){
+            on != on;
+        }
         pot = adc1_get_raw(ADC1_CHANNEL_7);
         if(abs(pot-prev >= 10)){
             brightness = ((float) pot) / 4095.0f * 8191.0f;
             if(brightness < 0) brightness = 0;
             if(brightness > 8191) brightness = 8191;
         }
-        printf("Potentiometer value %i\n", pot);
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, brightness));
-        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        if(on){
+            ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, brightness));
+            ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        }else{
+            ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0));
+            ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        }
     }
 }
