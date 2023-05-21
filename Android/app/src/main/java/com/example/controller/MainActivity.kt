5efa8fc1e6a9cd7c281e4f2b2bee6a658c1f569c
Justus Ojala <justus.ojala@aalto.fi>
2023-05-21 19:55:41 +0300

    Some Bluetooth stuff

--- Android/app/src/main/java/com/example/controller/MainActivity.kt
+++ Android/app/src/main/java/com/example/controller/MainActivity.kt
@@ -2,9 +2,24 @@ package com.example.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.bluetooth.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
+    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+
+    private fun btCB(){
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        val blM: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val blA: BluetoothAdapter? = blM.adapter
+
+        if(blA?.isEnabled == false){
+
+        }
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
