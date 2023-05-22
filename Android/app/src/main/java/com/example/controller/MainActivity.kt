fa51876733386d1e4fda4d24a64575728ff5c512
Justus Ojala <justus.ojala@aalto.fi>
2023-05-22 14:36:54 +0300

    Maybe sorta functions

--- Android/app/src/main/java/com/example/controller/MainActivity.kt
+++ Android/app/src/main/java/com/example/controller/MainActivity.kt
@@ -4,23 +4,54 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.bluetooth.*
 import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.TextView
+import java.io.OutputStream
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
-
-    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-
-    private fun btCB(){
-
-    }
+    private var lamps: List<BluetoothDevice>? = emptyList()
+    private var oStream: OutputStream? = null
+    private var brightness: Int = 255
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener{turnOff()}
+        findViewById<Button>(R.id.button2).setOnClickListener{turnOn()}
+        //findViewById<Button>(R.id.seekBar).setOnClickListener{turnOn()}
+
         val blM: BluetoothManager = getSystemService(BluetoothManager::class.java)
         val blA: BluetoothAdapter? = blM.adapter
 
-        if(blA?.isEnabled == false){
+        if(checkSelfPermission("android.permission.BLUETOOTH_SCAN") != PERMISSION_GRANTED) return
+        if(checkSelfPermission("android.permission.BLUETOOTH_CONNECT") != PERMISSION_GRANTED) return
 
+        val pairedDevices: Set<BluetoothDevice>? = blA?.bondedDevices
+        lamps = pairedDevices?.filter{it.name == "IOT_LAMP"}
+        if(lamps == null || lamps?.isEmpty() == true){
+            return
         }
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val socket: BluetoothSocket = lamps?.first()!!.createRfcommSocketToServiceRecord(UUID.randomUUID())
+        oStream = socket.outputStream
+    }
+
+    private fun turnOn(){
+        val message: ByteArray = ByteArray(2)
+        message[0] = 1
+        message[1] = -1
+        oStream!!.write(message)
+    }
+
+    private fun turnOff(){
+        oStream!!.write(0)
+    }
+
+    private fun setBrightness(){
+        brightness = 255
     }
 }
